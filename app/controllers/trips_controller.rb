da42fa98aec7e40d659abbeafab138054ca511da
Luke Ledet <luke@lootbox.org>
2010-10-15 20:20:26 -0500

    Setup basic trip controller and testing views

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,46 @@
 class TripsController < ApplicationController
+  def index
+    # TODO Should be users' trips
+    @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(params[:trip])
+
+    if @trip.save
+      redirect_to @trip, :notice => 'Trip created.'
+    else
+      render :action => :new
+    end
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+
+    if @trip.update_attributes(params[:trip])
+      redirect_to @trip, :notice => 'Trip successfully updated.'
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @trip = Trip.find(params[:id])
+    @trip.status = 'deleted'
+    @trip.save
+
+    redirect_to(trips_url)
+  end
 end
