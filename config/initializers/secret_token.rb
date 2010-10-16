79401b5fadc6f7098eb0ea7933a09332a3bb6e1b
Luke Ledet <luke@lootbox.org>
2010-10-15 19:04:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planitshareit::Application.config.secret_token = '94af6a20e5e843ef2fd46b2a7da1968f0beebaf7de3b0c2292e6036cfac22178ff7a0ee9b8c0584d633bb3ab31ee9508f71484b06774ca5780bf5ba877025598'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Planitshareit::Application.routes.draw do
+  devise_for :users
+
+end
