06324a113a7616c63778113d43157eb22729dad3
Luke Ledet <luke@lootbox.org>
2010-10-15 21:46:08 -0500

    Added a before create callback and a method to get the days of a trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,16 @@ class Trip < ActiveRecord::Base
   validates_presence_of :end_date
 
   default_scope where(:status => 'active')
+
+  before_create :set_title
+
+  def days
+    (start_date.to_date..end_date.to_date).to_a
+  end
+
+  private
+
+  def set_title
+    self.title = "#{destination_city} #{start_date.year}"
+  end
 end
