573531acb273640cb12d249cb95e8e406e499bad
Adam Clarke <vaxinate@Adam-Clarkes-MacBook.local>
2010-10-15 21:56:29 -0500

    mplemented new and create for events

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,8 @@ class Trip < ActiveRecord::Base
   validates_presence_of :destination
   validates_presence_of :start_date
   validates_presence_of :end_date
+  
+  has_many :events
 
   default_scope where(:status => 'active')
 end
