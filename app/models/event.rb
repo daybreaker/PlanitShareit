d8809f50969c3509fbccbf14876bbd9fb8850392
Luke Ledet <luke@lootbox.org>
2010-10-16 14:36:09 -0500

    Renamed open events to unscheduled to avoid overwriting an existing method and prevent unmapped events from causing javascript errors

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,6 @@ class Event < ActiveRecord::Base
   belongs_to :trip
 
   scope :for_date, lambda {|x| where('DATE(start_date)=?', x)}
-  scope :open, where(:start_date => nil)
+  scope :unscheduled, where(:start_date => nil)
+  scope :mapped, where('latitude IS NOT NULL')
 end
