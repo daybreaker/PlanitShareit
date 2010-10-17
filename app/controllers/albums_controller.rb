d46fd5c7864fd68692d3d3a57452eeaba91f2224
Luke Ledet <luke@lootbox.org>
2010-10-17 17:09:35 -0500

    Image galleries

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -18,6 +18,12 @@ class AlbumsController < ApplicationController
 
   def create
     @album = @trip.albums.new(params[:album])
+
+    if @album.save
+      redirect_to [@trip, @album], :notice => 'Album successfully created.'
+    else
+      redirect_to @trip, :alert => 'Your album could not be saved.'
+    end
   end
 
   def edit
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,13 @@
 class Image < ActiveRecord::Base
+  mount_uploader :file, ImageUploader
+
+  belongs_to :album
+
+  def next
+    Image.where("images.id > ? AND images.album_id=?", self.id, self.album_id).order("images.id ASC").first
+  end
+
+  def previous
+    Image.where("images.id < ? AND images.album_id=?", self.id, self.album_id).order("images.id DESC").first
+  end
 end
